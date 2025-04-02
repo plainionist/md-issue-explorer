889a6abc5bca11ea2c248cc1511707a3122f9136
plainionist <mail@plainionist.net>
2025-04-02 11:12:45 +0200

    watcher added

--- src/extension.ts
+++ src/extension.ts
@@ -31,8 +31,6 @@ export function activate(context: vscode.ExtensionContext) {
 
       const doc = await vscode.workspace.openTextDocument(filePath);
       await vscode.window.showTextDocument(doc);
-
-      provider.refresh();
     }),
 
     vscode.commands.registerCommand("issueExplorer.deleteIssue", async (item: { resourceUri: vscode.Uri }) => {
@@ -40,10 +38,17 @@ export function activate(context: vscode.ExtensionContext) {
 
       if (confirmed === "Yes") {
         fs.unlinkSync(item.resourceUri.fsPath);
-        provider.refresh();
       }
     })
   );
+
+  const watcher = vscode.workspace.createFileSystemWatcher(`${issuesFolder}/*.md`);
+
+  watcher.onDidChange(() => provider.refresh());
+  watcher.onDidCreate(() => provider.refresh());
+  watcher.onDidDelete(() => provider.refresh());
+
+  context.subscriptions.push(watcher);
 }
 
 export function deactivate() {}
